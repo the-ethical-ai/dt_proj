b0e4d4379db85e4a1e7544b8adfeedfca68eab51
Saikot Das Joy <saikotavi@gmail.com>
2023-06-05 22:41:57 +0200

    preprocess.py file completed , but review necessary

--- preprocess.py
+++ preprocess.py
@@ -2,24 +2,34 @@
 This file contains a function that clean the data and handles initial feature
 engineering.
 '''
-
 #### Importing required packages
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 ### Function definition
-def preprocess(X: pd.DataFrame) -> pd.DataFrame
-    ### Remove/Replace NAs (if 0, NaN, or empty, replace with mean value)
 
+def preprocess(X: pd.DataFrame) -> pd.DataFrame:
+    # Remove duplicates
+    data_wo_dup = X.drop_duplicates()
 
-    ### Remove Duplicates
+    # Remove rows with missing values
+    data_wo_na = data_wo_dup.dropna()
 
-print("Hello")
-    ### Drop the source column
+    # Remove the "source" column
+    df_afterremove = data_wo_na.drop("source", axis=1)
 
+    # Create a new column "US_resident" based on the "country" column
+    df_afterremove['US_resident'] = np.where(df_afterremove["country"] == "US", 1, 0)
 
-    ### Encode the countries as US or not US
+    # Remove the "country" column
+    df_final = df_afterremove.drop("country", axis=1)
 
+    # Scale the remaining features using StandardScaler
+    scaler = StandardScaler()
+    df_final_scaled = scaler.fit_transform(df_final)
 
+    # Convert the scaled array back to a DataFrame
+    df_final_final = pd.DataFrame(df_final_scaled)
 
-    ### Scaling the numeric features (StandardScaler)
+    return df_final_final
