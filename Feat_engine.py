a2bb8fd87b0cba3ea2e2ed335a9d5ff1e5409cfa
Tyler Chang <tchang343w@gmail.com>
2023-06-05 17:20:39 +0200

    added the bucket selection with rounded categories

--- Feat_engine.py
+++ Feat_engine.py
@@ -19,7 +19,7 @@ def feature_engineering(X: pd.DataFrame) -> pd.DataFrame
     psychopathy_Avg = psych_df.mean(axis = 1).round(3),
     machievellianism_Avg = mach_df.mean(axis = 1).round(3)
 
-    # Making the new columns for the dataframe
+    # Making the new columns for the average score per trait
     X['Narcissism_Avg'] = narcissism_Avg
     X['Psychopathy_Avg'] = psychopathy_Avg
     X['Machiavellianism_Avg'] = machievellianism_Avg
@@ -27,3 +27,22 @@ def feature_engineering(X: pd.DataFrame) -> pd.DataFrame
 
     ### Buckets (keep as integers) (new column per trait)
     ### Round to closest integer (<.5, down; >=.5, up)
+
+    narc_cats = []  # short for narcissism_categories
+    psych_cats = [] # same idea
+    mach_cats = []  # same idea
+
+    # Rounding each score and adding the rounded value to the dataframe as a new column
+    for i in range(len(narcissism_Avg)):
+        narc_cats.append(narcissism_Avg[i].round())
+        psych_cats.append(psychopathy_Avg[i].round())
+        mach_cats.append(machievellianism_Avg[i].round())
+
+    # Adding the rounded averages (categories) as new columns
+    X['Narcissism_Category'] = narc_cats
+    X['Psychopathy_Category'] = psych_cats
+    X['Machiavellianism_Category'] = mach_cats
+
+
+    ### Returning the dataframe
+    return X
