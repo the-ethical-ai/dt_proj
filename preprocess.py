320469bc31b6d4d230918dc41f803cf39bfaf14b
Saikot Das Joy <saikotavi@gmail.com>
2023-06-06 10:40:56 +0200

    Updated Preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -24,14 +24,15 @@ def preprocess(X: pd.DataFrame) -> pd.DataFrame:
 
     # Remove the "country" column
     df_final = df_afterremove.drop("country", axis=1)
-    column_names = list(df_final.columns[:-1])
-    # Scale the remaining features using StandardScaler
-    scaler = StandardScaler()
-    df_final_scaled = scaler.fit_transform(df_final.iloc[:, :-1])
+    # column_names = list(df_final.columns[:-1])
+    # # Scale the remaining features using StandardScaler
+    # scaler = StandardScaler()
+    # df_final_scaled = scaler.fit_transform(df_final.iloc[:, :-1])
 
-    # Convert the scaled array back to a DataFrame
-    df_final_final = pd.DataFrame(df_final_scaled,columns=column_names)
-    # final_dataframe = df_final_final.merge(df_final[["US_resident"]])
-    final_dataframe = df_final_final.merge(df_final[["US_resident"]], left_index=True, right_index=True)
+    # # Convert the scaled array back to a DataFrame
+    # df_final_final = pd.DataFrame(df_final_scaled,columns=column_names)
 
-    return final_dataframe
+    # final_dataframe = df_final_final.merge(df_final[["US_resident"]], left_index=True, right_index=True)
+
+    # return final_dataframe
+    return df_final
