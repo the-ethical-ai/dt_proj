7ac13eeb7574438039a53724a03b088fbe388b77
Tyler Chang <tchang343w@gmail.com>
2023-06-07 15:46:07 +0200

    created eval_model function and modified dictionary returned by model.py

--- performance_eval.py
+++ performance_eval.py
@@ -1,38 +1,55 @@
 '''
 Makes predictions and evaulates the models.
 
-1. Checks for overfitting/underfitting (plotting--learning curves)
-2. Computes scores for accuracy, precision, recall, and f1
-3. Cross-entropy for loss function
-4. Check residuals
-
 All of these steps must be performed on all 3 models.
 '''
-### Import the model
+### Import the packages and models
+from model import model
+from sklearn.metrics import accuracy_score
 
-def eval_model(model):
+def eval_model(sets: dict) -> dict:
+    ### Getting each of the models
+    models = model(sets)
+    psych_model = models['Psychopathy_Model']
+    narc_model = models['Narcissism_Model']
+    mach_model = models['Machiavellianism_Model']
 
-    ### Step 1:
+    ### Getting the test sets
+    X_test_psych = sets['test_sets'][0]
+    y_test_psych = sets['test_sets'][3]
 
+    X_test_narc = sets['test_sets'][1]
+    y_test_narc = sets['test_sets'][4]
 
-
-
-    ### Step 2:
-
-
-
-
-    ### Step 3:
-
-
-
-
-
-    ### Step 4:
+    X_test_mach = sets['test_sets'][2]
+    y_test_mach = sets['test_sets'][5]
 
 
-def pred(user_answers):
+    ### Psychopathy Model Evaluation
+    y_pred_psych = psych_model.predict(X_test_psych)
+    psych_model_score = accuracy_score(y_pred_psych, y_test_psych)
+
+    ### Narcissism Model Evaluation
+    y_pred_narc = narc_model.predict(X_test_narc)
+    narc_model_score = accuracy_score(y_pred_narc, y_test_narc)
+
+    ### Machiavellianism Model Evaulation
+    y_pred_mach = mach_model.predict(X_test_mach)
+    mach_model_score = accuracy_score(y_pred_mach, y_test_mach)
+
+    return {'Psychopathy_Model_Accuracy': psych_model_score,
+            'Narcissism_Model_Accuracy': narc_model_score,
+            'Machiavellianism_Model_Accuracy': mach_model_score}
+
+
+def pred(sets: dict, user_answers: dict):
 
     ### Call the model
+    models = model(sets)
+    psych_model = models['Psychopathy_Model']
+    narc_model = models['Narcissism_Model']
+    mach_model = models['Machiavellianism_Model']
+
+
 
     ### RETURN 3 predicted classes based on user's answers.
