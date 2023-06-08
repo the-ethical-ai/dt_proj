a603543e20ac6a8de1aba6f6ec4b4961522400a5
laura schopp <Laura.Oberscheid@web.de>
2023-06-08 10:58:47 +0200

    Change order

--- darktriad/interface/app.py
+++ darktriad/interface/app.py
@@ -6,9 +6,9 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from darktriad.params import DATA_PATH
+from params import DATA_PATH
 from preprocess import preprocess
-from darktriad.ml_logic.Feat_engine import feature_engineering
+from Feat_engine import feature_engineering
 
 start_button_clicked = False
 
