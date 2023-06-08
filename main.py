cb10edeb9f7e68d137da57f054e53b6b7d376862
Tyler Chang <tchang343w@gmail.com>
2023-06-08 17:37:51 +0200

    saving main.py

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ from darktriad.ml_logic.model import model
 from darktriad.ml_logic.train_test import train_test
 from darktriad.ml_logic.performance_eval import eval_model, pred
 
+ticker = False
 
-def load():
+def prep():
     df = pd.read_csv("/Users/zemblanity/portfolio_projects/Dark_Triad/dark_triad/Data/SD3/data.csv",delimiter = '\t')
     df = preprocess(df)
     df = feature_engineering(df)
@@ -27,16 +28,23 @@ def load():
     print('train test split done')
     return train_test_Dict
 
-def save_model():
-    models = model(load())
+def make_models():
+    models = model(prep())
     return models
+
+ticker = True
+
+def save_model(models):
+    dump(models, 'final_model.joblib')
 
 def prediction(user_answers: list = None) -> list:
     print("\n⭐️ Use case: predict")
+    if ticker == False:
+        save_model(make_models())
     try:
         assert user_answers is not None
 
-        models = get_cached_model()
+        models = load('final_model.joblib')
         assert models is not None
         print('models done')
 
@@ -51,4 +59,4 @@ def prediction(user_answers: list = None) -> list:
 
 
 if __name__ == '__main__':
-    prediction([5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,0])
+    prediction(list(np.ones(27)) + [1])
