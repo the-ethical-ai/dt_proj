c02d77b6d54d61351989ab32448ff19d60470013
imge ildan <balerimge@gmail.com>
2023-06-12 11:54:17 +0200

    Add requirement installation to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
-# list dependencies from file
-#with open('requirements.txt') as f:
-#    content = f.readlines()
-#requirements = [x.strip() for x in content]
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
 
 setup(name='dt_proj',
       #description="package description",
       packages=find_packages(), # NEW: find packages automatically
-      #install_requires=requirements)
+      install_requires=requirements
 )
