ccc1d18bca68528936984ac8c931543910eb9480
laura schopp <Laura.Oberscheid@web.de>
2023-06-14 20:36:01 +0200

    changed text, fonts, text size, layout ...

--- darktriad/interface/info_page.py
+++ darktriad/interface/info_page.py
@@ -6,7 +6,7 @@ parent_path = Path(__file__).parent.parent
 def info_page():
     # Display an image
     st.markdown("")
-    st.markdown("The Dark Triad is a refers to the trio of the three personality traits - narcissism, psychopathy and Marchiavellianism.")
+    st.markdown(":red[The Dark Triad] refers to the trio of the three personality traits - narcissism, psychopathy and Marchiavellianism.")
     image_file = Path(parent_path, "images", "image_front.jpg").absolute().as_posix()
     st.image(image_file, caption="Dark Triad", use_column_width=True)
     
@@ -15,11 +15,11 @@ def info_page():
     # short descriptions
     links = "[Machiavellianism](https://en.wikipedia.org/wiki/Machiavellianism_(psychology))&nbsp;&nbsp;[Narcissism](https://en.wikipedia.org/wiki/Narcissism)&nbsp;&nbsp;[Psychopathy](https://en.wikipedia.org/wiki/Psychopathy)"
     st.markdown(f"""
-        - Not familiar with the terms? Check this out:&nbsp;{links}
+        - Not familiar with the **terms**? Check this out:&nbsp; {links}
         - Our aim is to provide you insights about your personality with just a few clicks!""")
     
     st.divider()
-    st.markdown("Disclaimer: Any personality disorder can only be diagnosed by mental health professionals.")
+    st.markdown("**Disclaimer: Any personality disorder can only be diagnosed by mental health professionals.**")
 
 
 
